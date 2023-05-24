a4bf112056d4acb6a6cde973d4c85b62b19edaa0
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-24 22:35:47 +0200

    setup App component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Start Game</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="score" :score="score"/>
 </template>
 
 <script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
 export default {
   name: "App",
+  components: {
+    Block,
+    Results,
+  },
   data() {
-    return {};
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+    };
+  },
+  methods: {
+    start() {
+      this.score = null;
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+    },
   },
 };
 </script>
@@ -19,5 +44,19 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  border: none;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 1.2em;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 20px;
+}
+button:disabled {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
